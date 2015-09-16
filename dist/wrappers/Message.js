da1bd942896eaf98db7321fce36b92f9f868edd9
Jared Lotti <jaredlotti@gmail.com>
2015-09-16 12:40:24 -0400

    ignore dist folder

--- dist/wrappers/Message.js
+++
@@ -1,9 +0,0 @@
-var Message = (function () {
-    function Message(level, message, exception) {
-        this.level = level;
-        this.message = message;
-        this.exception = exception;
-    }
-    return Message;
-})();
-exports.Message = Message;
